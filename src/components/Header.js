4fcdc1258a22e83d65856c5790399924c9b75f55
lordrichard6 <paulolopesreizinho@gmail.com>
2020-04-17 11:31:58 +0200

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+
+class Header extends Component {
+    render() {
+        return (
+            <div className="container">
+                <header className="header">
+                    Header part
+                </header>
+                <div className="content">
+                    <nav className="sidebar">
+                        Navigation
+                    </nav>
+                </div>
+            </div>
+        )      
+    }
+}
+
+export default Header;
