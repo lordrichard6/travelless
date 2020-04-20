707bcb4ae4531b21c889ca91b3ad94c92ae698e0
lordrichard6 <paulolopesreizinho@gmail.com>
2020-04-20 11:16:39 +0200

    download icons

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,25 @@
 import React, { Component } from 'react';
 
+import logo from '../img/logo.png'
+
 
 class Header extends Component {
     render() {
         return (
             <div className="container">
                 <header className="header">
-                    Header part
+                    <img src={logo} alt="logo" className="logo"/>
+                    <form action="#" className="search">
+                        <input type="text" className="search__input" placeholder='Search Hotels'/>
+                    </form>
                 </header>
                 <div className="content">
                     <nav className="sidebar">
-                        Navigation
+                        Navigation 
                     </nav>
+                    <main className="hotel-view">
+                        Hotel View
+                    </main>
                 </div>
             </div>
         )      
